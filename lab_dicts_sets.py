6f56971e6388e5fdac143f83c9c3ce324074c5d2
bahiyyah_4 <bahiab144@gmail.com>
2022-10-16 19:53:23 +0300

    add lab

--- lab_dicts_sets.py
+++ lab_dicts_sets.py
@@ -1,15 +1,18 @@
 # - Create a variable to hold the values of Nestle products (use a dicitionary)
-Nestle_products = {"KitKat": "34-456-432", "Nescafe": "14-106-132",
-                   "Maggi": "9-960-312", "Nido": "44-506-003"}
+Nestle_products = {"KitKat": 34456432, "Nescafe": 14106132,
+                   "Maggi": 9960312, "Nido": 44506003}
 
 # Create a variable to hold the values of Unilever products (Use a dictionary)
-Unilever_products = {"Lipton": "23-456-000", "Breyers": "1-235-891",
-                     "HellManns": "17-241-412", "Marmite": "11-715-324"}
+Unilever_products = {"Lipton": 23456000, "Breyers": 1235891,
+                     "HellManns": 17241412, "Marmite": 11715324}
 # - Print each product sold by Unilever and the sales figures / numbers  for that product.
-
+for key, value in Unilever_products.items():
+    print(f"{key}:{value} US Dollars ")
 
 print(f"the sales of Unilever: {Unilever_products}")
 # - Print each product sold by Nestle and the sales figures / numbers  for that product.
+for key, value in Nestle_products.items():
+    print(f"{key}:{value} US Dollars ")
 
 print(f"the sales of  Nestle: { Nestle_products}")
 # - Print which of the companies has more products that the other company.
@@ -29,21 +32,18 @@ print(f"the top selling in Nestle is {max(Nestle_products)}")
 # - Print the top selling product from Unilever with sales figures.
 print(f"the top selling in Unilever is {max(Unilever_products)}")
 # - Using Sets & a loop, print all the cities Unilever & Nestle sell their products in.
-'''unilever_nestle = []
-for i in (Unilever_products, Nestle_products):
-    i = unilever_nestle.union(set(i))
-    print(i)'''
-
-#print(set().union(Unilever.keys(), Nestle.keys()))
 # - Using Sets & a loop, print the cities that both Nestle & Unilver sell in common.
 Nestle = {"Saudi Arabia", "Oman", "Kuwait", "Egypt", "Jordan", "Sudan"}
 Unilever = {"Saudi Arabia", "Kuwait", "Iraq",
             "Morocco", "Yemen", "United Emirates"}
-
 print(Unilever.union(Nestle))
 print(Unilever.intersection(Nestle))
 print(Unilever.difference(Nestle))
-a = {x for x in Unilever if x not in Nestle}
-print(a)
-c = {x for x in Unilever if x in Nestle}
-print(c)
+union_value = {x for x in Unilever | Nestle}
+print(union_value)
+intersection_value = {x for x in Unilever & Nestle}
+print(intersection_value)
+difference_value = {x for x in Unilever - Nestle}
+print(difference_value)
+
+
